7267e25be0c8dd15233171868e35197b61b2369e
Ayo Akindele <a.akindele01@gmail.com>
2021-04-29 01:36:06 -0600

    update DarkModeToggle functionality

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,19 @@ export default {
 
 </script>
 
-<style>
+<style lang="scss">
+$smooth-speed: 0.3s;
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+body {
+  transition: $smooth-speed;
+}
+
+body.dark-mode {
+  background: #212121;
 }
 </style>
--- src/components/DarkModeToggle.vue
+++ src/components/DarkModeToggle.vue
@@ -1,3 +1,118 @@
 <template>
-  <div></div>
+<!-- dmtoggle from https://connective.dev/ -->
+  <div class="dmtoggle" v-on:click="toggleDarkTheme">
+    <div class="arc"></div>
+    <div class="darc"></div>
+    <div class="ray one"></div>
+    <div class="ray two"></div>
+    <div class="ray three"></div>
+    <div class="ray four"></div>
+    <div class="ray five"></div>
+    <div class="ray six"></div>
+    <div class="ray seven"></div>
+    <div class="ray eight"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+
+// for smooth toggle transitions, this component
+// requires body selector (outside of immediate scope)
+// to have a transition property of $smooth-speed seconds
+
+$toggle-dimensions: 48px;
+$arc-dimensions: 16px;
+$ray-height: 2px;
+$ray-width: 6px;
+$no-opacity: 0;
+$light-opacity: 0.25;
+$full-opacity: 1;
+$ray-dist-from-sun: 14px;
+$smooth-speed: 0.3s;
+$quick-speed: 0.2s;
+$light-color: #f5f5f5;
+$light-bg-color: white;
+$dark-color: #424242;
+$darker-color: #212121;
+
+.dmtoggle {
+  width: $toggle-dimensions;
+  height: $toggle-dimensions;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: $light-opacity;
+  cursor: pointer;
+  transition: transform $smooth-speed, opacity $quick-speed;
+}
+
+body.dark-mode .dmtoggle {
+  transform: rotate(-45deg);
+}
+
+.dmtoggle:hover {
+  opacity: $full-opacity;
+}
+
+.dmtoggle div {
+  position: absolute;
+  background: $dark-color;
+  transition: transform $smooth-speed, background $smooth-speed, opacity $smooth-speed;
+}
+
+body.dark-mode .dmtoggle div {
+  background: $light-color;
+}
+
+.dmtoggle .arc, .dmtoggle .darc {
+  width: $arc-dimensions;
+  height: $arc-dimensions;
+  border-radius: $arc-dimensions;
+}
+
+body.dark-mode .dmtoggle .arc {
+  transform: scale(1.2);  // make the crescent moon circle bigger
+}
+
+.dmtoggle .darc {
+  background: $light-bg-color; // hide the "moon bite" in light mode
+  transform: translateX(24px);  // "moon bite" moves out and to the right
+}
+
+body.dark-mode .dmtoggle .darc {
+  background: $darker-color;  // show the "moon bite" in dark mode
+  transform: translateX(6px); // offset .darc by 6px to make crescent moon shape
+}
+
+.dmtoggle .ray {
+  width: $ray-width;
+  height: $ray-height;
+  border-radius: $ray-height;
+}
+
+.dmtoggle .ray.one { transform: rotate(0deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.two { transform: rotate(45deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.three { transform: rotate(90deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.four { transform: rotate(135deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.five { transform: rotate(180deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.six { transform: rotate(225deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.seven { transform: rotate(270deg) translateX($ray-dist-from-sun); }
+.dmtoggle .ray.eight { transform: rotate(315deg) translateX($ray-dist-from-sun); }
+
+body.dark-mode .dmtoggle .ray {
+  transform: scaleX(0.01);  // make the rays "invisibly" thin
+  opacity: $no-opacity;
+}
+
+</style>
+
+<script>
+export default {
+  methods: {
+    toggleDarkTheme () {
+      document.body.classList.toggle('dark-mode')
+    }
+  }
+}
+</script>
