6cb7d444263b0708e0ea8210cbdceb528b025b1f
Ayo Akindele <a.akindele01@gmail.com>
2021-06-02 03:52:01 -0600

    update Contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container" id="contact">
+    <h1 class="sh_container sh_center"><span class="section-heading">Contact Me</span></h1>
+    <div class="center-flex">
+      <p>If you have any questions or if you want to talk, feel free to get in touch with me.</p>
+      <a href="mailto:a.akindele01@gmail.com"><button class="active-cta">Send Me a Message</button></a>
+    </div>
+  </div>
+  <div class="spacer"></div>
+</template>
+
+<style scoped>
+
+.center-flex {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  /* background: #72c4c088; */
+}
+
+p {
+  text-align: center;
+  width: 70%;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.4rem;
+  letter-spacing: 0.05rem;
+}
+
+
+
+</style>
