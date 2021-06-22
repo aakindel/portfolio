61ad04e3751a59342544059b0170cb47347e5087
Ayo Akindele <a.akindele01@gmail.com>
2021-06-22 10:45:55 -0600

    update favicon and title

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 // vue config created to globally load sass styles
 
 module.exports = {
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Ayo Akindele';
+        return args;
+      });
+  },
   css: {
     loaderOptions: {
       sass: {
