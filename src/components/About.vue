cc9d403a14aaf0ef84ac000b3308e85f4fd34225
Ayo Akindele <a.akindele01@gmail.com>
2022-06-27 14:24:10 -0600

    updated resume

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="container" id="about">
-    <h1 class="sh_container sh_right"><span class="section-heading">About Me</span></h1>
+    <h1 class="sh_container sh_right">
+      <span class="section-heading">About Me</span>
+    </h1>
     <div class="about-content">
       <div class="text-box">
-        <p>I'm currently pursuing my Bachelor's degree at the University of Alberta with a
-          specialization in Computing Science and a minor in Business. <br /> <br />
-          I love tackling
-          interesting problems and developing elegant software solutions that both
-          solve the immediate problem and solve potential problems before they ever come up.
-          I'm passionate about pursuing creativity and about collaborating with others to improve upon
-          the past while building for the future. <br /> <br />
-          Here are some of the technologies and tools that I have experience with:
+        <p>
+          I got my Bachelor's degree with a specialization in Computing Science
+          and a minor in Business from the University of Alberta. <br />
+          <br />
+          I love tackling interesting problems and developing elegant software
+          solutions that both solve the immediate problem and solve potential
+          problems before they ever come up. I'm passionate about pursuing
+          creativity and about collaborating with others to improve upon the
+          past while building for the future. <br />
+          <br />
+          Here are some of the technologies and tools that I have experience
+          with:
         </p>
         <ul class="recent-tech">
-          <li :key="tech" v-for="tech in recentTech">{{tech}}</li>
+          <li :key="tech" v-for="tech in recentTech">{{ tech }}</li>
         </ul>
-        <a href="/resume.pdf" target="_blank"><button class="active-cta">View My Resume</button></a>
+        <a href="/resume.pdf" target="_blank"
+          ><button class="active-cta">View My Resume</button></a
+        >
       </div>
       <div class="img-box">
         <img class="pic" src="../assets/portfolio_pic.jpg" alt="About Me" />
@@ -27,23 +35,22 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       recentTech: [
-        'React',
-        'Vue',
-        'Android SDK',
-        'JavaScript',
-        'HTML & CSS',
-        'Python'
+        "React",
+        "Vue",
+        "Android SDK",
+        "JavaScript",
+        "HTML & CSS",
+        "Python"
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-
 .text-box {
   font-size: 16px;
   line-height: 160%;
@@ -60,12 +67,14 @@ export default {
   /* css filter generator for coloring images: https://codepen.io/sosuke/pen/Pjoqqp */
   width: 300px;
   border-radius: 5px;
-  filter: invert(13%) sepia(89%) saturate(37%) hue-rotate(227deg) brightness(var(--light-image-tint)) contrast(90%);
+  filter: invert(13%) sepia(89%) saturate(37%) hue-rotate(227deg)
+    brightness(var(--light-image-tint)) contrast(90%);
   transition: 0.3s;
 }
 
 .pic:hover {
-  filter: invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg) brightness(100%) contrast(100%);
+  filter: invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg) brightness(100%)
+    contrast(100%);
 }
 
 .recent-tech {
@@ -74,13 +83,13 @@ export default {
 }
 
 .recent-tech {
-    margin-top: 15px;
+  margin-top: 15px;
 }
 
 .recent-tech li::before {
-    content: "▹";
-    margin-right: 12px;
-    line-height: 12px;
+  content: "▹";
+  margin-right: 12px;
+  line-height: 12px;
 }
 
 button {
@@ -106,7 +115,8 @@ button {
     margin: auto 0;
   }
 
-  .img-box, .text-box {
+  .img-box,
+  .text-box {
     width: 48%;
   }
 
@@ -126,5 +136,4 @@ button {
     margin-top: 125px;
   }
 }
-
 </style>
